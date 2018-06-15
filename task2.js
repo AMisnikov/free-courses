233683af2c563c8f77e239512129f32bc3b2fce2
Alexander Misnikov <www.start@inbox.ru>
2018-06-16 10:38:20 +1100

    task2.js ver.4

--- task2.js
+++ task2.js
@@ -2,21 +2,13 @@
 
 		function doublePrefix(string) {
 
-			if (string == undefined) {
-				process.stdout.write("Использование программы: node task2.js 'строка'");
+			if (string === undefined || string.length > 100) {
+				process.stdout.write("Необходимо ввести строку длиной от 1 до 100 символов");
 				return;
 			}
 			
 			let str = string.toLowerCase();
 			
-
-			if (str.length < 1 || str.length > 100) {
-
-				process.stdout.write("Необходимо ввести строку длиной от 1 до 100 символов");
-				return;
-			}
-
-
 			for (let k = 0; k < str.length; k++) {
 				if (str[k].charCodeAt() > 122 || str[k].charCodeAt() < 97) {
 
@@ -24,52 +16,28 @@
 					return;
 				}
 			}
-			
-
+		
 			let search = "";
-			let substr = [];
+			let result = "Совпадений не найдено";
 			
-
 			for (let i = 0; i < str.length; i++ ) {
 				search += str[i];
 
-				
 				for (let j = 0, counter = 0; j < str.length - (search.length - 1); j++) {
 
 					let foundPos = str.indexOf(search, j);
 
-					if (foundPos !== -1) { 
-						
-						counter++;
-
-					} 
+					if (foundPos !== -1) counter++; 
 
 					if(counter > 1) {
 
-						substr.push(search);
-						break;
+						result = search;
+						break;	
 					}
-
-					
-					
-
 				}
-
 			}
-
-
-				if (substr.length !== 0) {
-
-				process.stdout.write(substr[substr.length - 1]);
-				return substr[substr.length - 1];
-
-			} else {
-
-				process.stdout.write("Повторяющихся строк не найдено");
-				return;
-			}
-
+				
+			process.stdout.write(result);
 		}
-
-
+		
 doublePrefix(process.argv[2]);
